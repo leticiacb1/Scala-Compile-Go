065059be771d76f9643d0be458e30bb4732e3ae6
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-08-20 15:26:49 -0300

    Solving operations without problem. Needs improve for better performance.

--- main.py
+++ main.py
@@ -3,6 +3,35 @@ from abc import ABC, abstractmethod
 import sys 
 import re
 
+class Node():
+
+    def __init__(self, element : str , element_before : object , isroot = False):
+        
+        self.type = None
+        self.error = False
+        self.is_root = isroot
+        
+        self.element = element
+        self.element_before = element_before
+
+    def evaluate(self):
+        if(self.element.isdigit()):
+            self.type =  'number'
+        else:
+            self.type =  'operator'
+
+    def check(self):
+
+        if(not self.is_root):
+            if (self.type == self.element_before.type):
+                self.error = True
+            else:
+                self.error = False
+
+        else:
+            if(self.is_root and self.type == 'operator'):
+                self.error = True 
+
 def simple_calculator(string_input):
    
     elements = []
@@ -34,13 +63,38 @@ def simple_calculator(string_input):
             idx+=1
     
     print(f" Lista de elementos : {elements}")
-    print(eval(''.join(elements)))
 
-    # Criando nós da árvore:
+    tree = []
+    for idx , var in enumerate(elements):
+        
+        if(idx == 0):
+            if(var.isdigit()):
+                
+                node = Node(var , '' , True)
+                node.evaluate()
+                node.check()
 
+                tree.append(node)
+            else:
+                raise Exception("Sorry, wrong operation")
+        
+        else:
+            
+            node = Node(var , tree[idx-1])
+            node.evaluate()
+            node.check()
 
+            tree.append(node)
 
-    return 0
+        if (node.error):
+            raise Exception("Sorry, wrong operation")
+
+    if(tree[-1].type == 'operator'):
+        raise Exception("Sorry, wrong operation")
+
+    result = eval(''.join(elements))
+    print(f"\n\t > Resultado : {result}")
+    return result
     
 if __name__ == '__main__':
     std_input = (sys.argv)[1:]
