fb688d41d38c0ea4a989b1b4bf799319a03aa35e
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-08-21 17:31:11 -0300

    Adjusting gitignore

--- Hello.scala
+++ Hello.scala
@@ -1,6 +1,6 @@
 
 object Hello {
     def main(args: Array[String]) = {
-        println("Hello, world")
+        printf("Hello world , %s\n", args(0))
     }
 }
---
+++ main.scala
@@ -0,0 +1,9 @@
+
+class Token (_type : String , _value : Int) {
+  val type : String = _type 
+  val integer : Int = _value
+}
+
+class Tokenizer {
+
+} 
