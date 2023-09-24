07fd4e1e0619ca8bcbaa85bb224f7f90ad1fddd8
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-09-24 13:12:03 -0300

    Modifying tokenizer to generate end of line token, function token and token identifier

--- Tokenizer.scala
+++ Tokenizer.scala
@@ -9,6 +9,9 @@ class Tokenizer ( _source : String){
   var source : String = _source
   var position : Int = 0
   var next : Token = new Token("", 0)
+  var reserved_words : Map[String, Map[String, Any]] = Map(
+    "println" -> Map("type" -> Types.PRINTLN , "value" -> Values.PRINTLN),
+  )
 
   def selectNext() : Unit = {
 
@@ -71,6 +74,12 @@ class Tokenizer ( _source : String){
                         break;
                     }
 
+                    case Types.EQUAL => {
+                        next = new Token(_type = Types.EQUAL.toString , _value = Values.EQUAL)
+                        position +=1
+                        break;
+                    }
+
                     case Types.OPEN_PARENTHESES => {
                         next = new Token(_type = Types.OPEN_PARENTHESES.toString , _value = Values.PARENTHESES)
                         position +=1
@@ -83,7 +92,33 @@ class Tokenizer ( _source : String){
                         break;
                     }
 
-                    case x if x.isWhitespace => {
+                    case Types.END_OF_LINE => {
+                        next = new Token(_type = Types.END_OF_LINE.toString , _value = Values.END_OF_LINE)
+                        position +=1
+                        break;
+                    }
+
+                    case c if c.forall(_.isLetter) => {
+                        var value_str : String = ""
+                        val pattern: Regex = "[a-zA-Z0-9_]+".r
+
+                        breakable {
+                            while((position < source.length) && (pattern.matches(c))){
+                                value_str += c
+                                position += 1
+                            }
+                        }
+
+                        if (reserved_words.contains(value_str)){
+                            next = new Token(_type = reserved_words("println")("type") , _value = reserved_words("println")("value"))
+                        }else{
+                            next = new Token(_type = Types.IDENTIFIER , _value = value_str)
+                        }
+
+                        break;
+                    }
+
+                    case c if c.isWhitespace => {
                         position += 1
                     }
 
