1a1930231b428e7b9b37e21af03117497901a1dc
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-09-24 11:54:52 -0300

    Change toString token function.

--- Token.scala
+++ Token.scala
@@ -11,12 +11,24 @@ class Token (var _type : String , var _value : Int){
             "( " + _type + " , " + "PLUS " + ")"
         case "-"   => 
             "( " + _type + " , " + "MINUS " + ")"
+        case "*"   => 
+            "( " + _type + " , " + "TIMES " + ")"
+        case "/"   => 
+            "( " + _type + " , " + "BAR " + ")"
+        case "=" =>  
+            "( " + _type  + " , " + " EQUAL " + ")"
         case "EOF" =>  
             "( " + _type + " , " + "EOF " + ")"
         case "(" =>  
             "( PARENTHESES " + " , " + " OPEN_PARENTHESES " + ")"
         case ")" =>  
             "( PARENTHESES " + " , " + " CLOSE_PARENTHESES " + ")"
+        case "\n" =>  
+            "( \\n" + " , " + " END_OF_LINE " + ")"
+        case "PRINTLN" => 
+            "( " + _value + " , " + _type + ")"
+        case "IDENTIFIER" => 
+            "( " + _value + " , " + _type + ")"
         case _     => 
             "( " + _type + " , " + "INVALID " + ")"
     }
