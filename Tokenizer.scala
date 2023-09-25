08bbe7263fa1768ef396a2d6c14589eae0e4c23b
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-09-25 12:06:45 -0300

    Add upercase print function.

--- Tokenizer.scala
+++ Tokenizer.scala
@@ -10,7 +10,7 @@ class Tokenizer ( _source : String){
   var source : String = _source
   var position : Int = 0
   var next : Token = new Token("", 0)
-  var reserved_words : List[String] = List("println")
+  var reserved_words : List[String] = List("println","Println")
 
   def selectNext() : Unit = {
 
--- teste.go
+++ teste.go
@@ -1,8 +1,11 @@
-// Comentario
-x = 5
-y = 3  // Comentario
+Println(1)
+x = 3+6/3   *  2 -+-  +  2*4/2 + 0/1 -((6+ ((4)))/(2)) // Teste // Teste 2
+y_1 = 3
+y_1 = y_1 + 2
 
-var_z = ( x + y ) / 2 +- 1
+z__ = x + y_1
 
-// Resposta : 
-println(var_z)
+
+// Saida final
+Println(x)
+Println(z__+1)
