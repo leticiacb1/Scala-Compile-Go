ba403af80be4c1317c8f47716988254109500a78
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-10-03 21:25:45 -0300

    Update tokenizer.

--- Tokenizer.scala
+++ Tokenizer.scala
@@ -10,7 +10,7 @@ class Tokenizer ( _source : String){
   var source : String = _source
   var position : Int = 0
   var next : Token = new Token("", 0)
-  var reserved_words : List[String] = List("Println")
+  var reserved_words : List[String] = List("Println" , "Scanln" , "for" , "if" , "else")
 
   def selectNext() : Unit = {
 
@@ -73,9 +73,62 @@ class Tokenizer ( _source : String){
                     }
 
                     case Types.EQUAL => {
-                        next = new Token(_type = Types.EQUAL.toString , _value = Values.EQUAL)
+                        
+                        if( source.charAt(position+1) ==  Types.EQUAL){
+                            next = new Token(_type = Types.EQUAL_COMP , _value = Values.EQUAL_COMP)
+                            position +=1
+                        }else{
+                            next = new Token(_type = Types.EQUAL.toString , _value = Values.EQUAL)
+                        }
+                        
                         position +=1
                         break;
+                    }
+                    
+                    case Types.BIGGER_THEN => {
+                        next = new Token(_type = Types.BIGGER_THEN.toString , _value = Values.BIGGER_THEN)
+                        position +=1
+                        break;
+                    }
+
+                    case Types.LESS_THAN => {
+                        next = new Token(_type = Types.LESS_THAN.toString , _value = Values.LESS_THAN)
+                        position +=1
+                        break;
+                    }
+
+                    case Types.NOT => {
+                        next = new Token(_type = Types.NOT.toString , _value = Values.NOT)
+                        position +=1
+                        break;
+                    }
+
+                    case Types.E => {
+
+                        if( source.charAt(position+1) ==  Types.E ){
+                            next = new Token(_type = Types.AND , _value = Values.AND)
+                            position +=2
+                            break;
+                        }else{
+                            next = new Token(_type = Types.INVALID , _value = Values.INVALID)
+                            position += 1
+                            break;
+                        }
+                        
+                    }
+
+                    case Types.O => {
+
+                        if( source.charAt(position+1) ==  Types.O ){
+                            next = new Token(_type = Types.OR , _value = Values.OR)
+                            position +=2
+                            break;
+                        }else{
+                            next = new Token(_type = Types.INVALID , _value = Values.INVALID)
+                            position += 1
+                            break;
+                        }
+                        
                     }
 
                     case Types.OPEN_PARENTHESES => {
@@ -90,8 +143,26 @@ class Tokenizer ( _source : String){
                         break;
                     }
 
+                    case Types.OPEN_KEY => {
+                        next = new Token(_type = Types.OPEN_KEY.toString , _value = Values.OPEN_KEY)
+                        position +=1
+                        break;
+                    }
+
+                    case Types.CLOSE_KEY => {
+                        next = new Token(_type = Types.CLOSE_KEY.toString , _value = Values.CLOSE_KEY)
+                        position +=1
+                        break;
+                    }
+
                     case Types.END_OF_LINE => {
                         next = new Token(_type = Types.END_OF_LINE.toString , _value = Values.END_OF_LINE)
+                        position +=1
+                        break;
+                    }
+
+                    case Types.SEMICOLON => {
+                        next = new Token(_type = Types.SEMICOLON.toString , _value = Values.SEMICOLON)
                         position +=1
                         break;
                     }
@@ -111,7 +182,21 @@ class Tokenizer ( _source : String){
                         }
                         
                         if (reserved_words.contains(value_str)){
-                            next = new Token(_type = Types.PRINTLN , _value = Values.PRINTLN)
+
+                            if(value_str == Types.PRINTLN){
+                                next = new Token(_type = Types.PRINTLN , _value = Values.PRINTLN)
+                            } else if(value_str == Types.SCANLN){ 
+                                next new Token(_type = Types.SCANLN , _value = Values.SCANLN)
+                            } else if(value_str == Types.IF){
+                                next new Token(_type = Types.IF , _value = Values.IF)
+                            } else if(value_str == Types.ELSE){
+                                next new Token(_type = Types.ELSE , _value = Values.ELSE)
+                            } else if(value_str == Types.FOR){
+                                next new Token(_type = Types.FOR , _value = Values.FOR)
+                            } else{
+                                throw new InvalidExpression("\n [TOKENIZER] Invalid function value : " + value_str)
+                            }
+
                         }else{
                              next = new Token(_type = Types.IDENTIFIER , _value = value_str)
                         }
