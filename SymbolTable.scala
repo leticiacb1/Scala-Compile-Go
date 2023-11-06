53a3e38398f6f1d9cf6c3c8a594a9552b221858c
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-11-06 18:46:58 -0300

    Create function get_position in SymbolTable.

--- SymbolTable.scala
+++ SymbolTable.scala
@@ -2,13 +2,15 @@ package table
 
 import errors.ExistingKey
 import errors.NonExistingKey
+import scala.languageFeature.postfixOps
 
 class SymbolTable (){
     
-    // table = { var : (value , type)}
+    // table = { var : (value , type , position)}
     //O tipo pode ser "INT" ou "STRING" .
 
-    var table : Map[String,(Any, String)] = Map.empty
+    var table : Map[String,(Any, String, Int)] = Map.empty
+    var position : Int = 0
 
     def getter(identifier : String): (Any, String) = {
         var value : Any  = ""
@@ -25,16 +27,24 @@ class SymbolTable (){
         (value,_type)
     }
 
+    def get_position(identifier : String) : Int = {
+        var result = table.get(identifier)
+        var position = result.get._3
+        return position
+    }
+
     def create(identifier : String , _type: String): Unit  ={
         if(table.get(identifier).isDefined){
             throw new ExistingKey(s"Chave já existente : $identifier")
         }else{
-            table = table + (identifier -> (0 , _type)) 
+            table = table + (identifier -> (0 , _type , 4 + position*4)) 
+            position += 1
         }
     }
 
     def setter(identifier : String, value : Any) : Unit = {
         var _type = table.get(identifier).get._2
-        table = table + (identifier -> (value, _type))
+        var _position = table.get(identifier).get._3
+        table = table + (identifier -> (value, _type, _position))
     }
 }
